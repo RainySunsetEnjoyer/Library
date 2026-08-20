330ffa4a5ce6973de8bffbfc6ea2e17e1a0d2290
RainySunsetGod <carl.mikaelsson@outlook.com>
2026-08-20 15:19:22 +0200

    Updated: List of Books

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BookList } from './components/books/book-list/book-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'books',
+        pathMatch: 'full'
+    },
+    {
+        path: 'books',
+        component: BookList
+    }
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/layout/navbar/navbar';
+import { BookList } from './components/books/book-list/book-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, BookList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
